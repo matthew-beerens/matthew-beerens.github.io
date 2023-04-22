ccc7be71ce4385f2de363ce560ed4b7cfe58af95
matthew-beerens <matthew.beerens@gmail.com>
2023-04-22 19:51:56 +1000

    initial layout of website with required pages

---
+++ portfolio/pages/components/project.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+class Projects extends Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return (
+            <section className="flex h-screen justify-center items-center">
+                <h1>THIS IS THE PROJECTS</h1>
+            </section>
+        );
+    }
+}
+
+export default Projects;
