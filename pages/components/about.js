1881f980a94f7747476598b1a2602209cb00f29f
matthew-beerens <matthew.beerens@gmail.com>
2023-04-24 23:52:35 +1000

    add content to sections of portfolio, included bootstrap icons

--- pages/components/about.js
+++ pages/components/about.js
@@ -10,13 +10,13 @@ class About extends Component
     render()
     {
         return (
-            <section id='about' className="section items-center p-12">
+            <section id='about' className="section items-center ">
+                <div className="flex flex-col h-screen w-screen justify-center items-center p-12">
                     <h2 className="text-5xl text-center m-10">ABOUT ME</h2>
-                    <div className="text-center">
-                        <p>
+                    <p className="text-center w-2/3">
                         I'm a computer science graduate from WSU in Sydney, passionate about web and software development, as well as cyber security. With a strong foundation in programming, I create functional and visually appealing applications that solve real-world problems. I also have experience in ethical hacking and penetration testing, ensuring that the software I build is secure. In my free time, I enjoy reading about and learning new technologies. Feel free to reach out and say hello!    
-                        </p>
-                    </div>
+                    </p>
+                </div>
             </section>
         );
     }
--- pages/components/expertise.js
+++ pages/components/expertise.js
@@ -10,14 +10,39 @@ class Expertise extends Component
     render()
     {
         return (
-            <section id="expertise" className="section items-center p-12">
-                    <h2 className="text-5xl text-center m-10">EXPERTISE</h2>
-                    <div className="text-center">
-                        <p>
-                            Hello, this is about me Hello, this is about me Hello, this is about me Hello, this is about me Hello, this is about me
-                            Hello, this is about me Hello, this is about me Hello, this is about me Hello, this is about me
-                        </p>
+            <section id="expertise" className="section items-center">
+                <div className="h-screen w-screen flex flex-col justify-center items-center">
+                <h2 className="text-5xl text-center m-10">EXPERTISE</h2>
+                    <div className="flex w-4/5 gap-12 justify-center">
+                        <section className="text-left p-4 w-1/2">
+                            <p>
+                                I have spent the last decade exploring a range of technologies which include
+                                web (frontend, backend), desktop and mobile applications, command-line tools, databases and operating systems.
+                            </p>
+                            <br/>                            
+                            <p>
+                                I've spent time learning and understanding the principles and practices of cybersecruity.
+                            </p>
+                            <br/>
+                            <p>
+                            I have learnt how to build and implement a plethora of network designs and have an indepth understanding of how they work.
+                            </p>
+                        </section>
+                        
+                        <ul className="text-left p-4 w-1/2 space-y-2">
+                            <li>Linux operating system and distributions (Redhat, Fedora, centOS, Kali)</li>
+                            <li>Scripting and automating tasks with Python/Bash</li>
+                            <li>Systems programming with C</li>
+                            <li>Web development with Django, React/Express, PHP</li>
+                            <li>Software Development using C++, C, Java, Python</li>
+                            <li>Database design and development</li>                                <li>Penetration testing using industry standard tools</li>
+                        </ul>
+                        
                     </div>
+                    <br/>
+                    <p className="text-neutral-400">I made this website using Next.js, Tailwind CSS and fullPage.js</p>
+                </div>
+                    
             </section>
         );
     }
